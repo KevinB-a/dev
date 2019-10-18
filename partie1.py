3a00d827a32773efbb1f1aaed9e29223e9e141a9
Kevin <kevin.billet.pro@gmail.com>
2019-10-18 09:18:53 +0200

    add exercise2

--- partie1.py
+++ partie1.py
@@ -1,4 +1,5 @@
 #exercise1 :the bigger number
+import math
 number=1
 number1=2
 number2=3
@@ -11,3 +12,19 @@ elif number2>number and number2>number1 and number2>number3: #compare number2 wi
     print("le nombre le plus grand est ",number2)
 else:
     print("le nombre le plus grand est",number3) # if other condition are false display last variable
+
+#exercise2 : age condition
+try:
+    age=int(input("please enter your age"))
+except ValueError:
+    print("you did not write anything")
+if age<0:
+    print("please enter a real age")
+elif math.sqrt(age).is_integer():
+    print("you age is a perfect square")
+if age>= 21:
+    print("you have access ")
+if age % 2==0:
+    print("your age is an even number")
+else:
+    print("you age is not a perfect square")
