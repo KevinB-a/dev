9c3c4cb4aa70560ca405830da6bc7b355b441598
Kevin <kevin.billet.pro@gmail.com>
2019-10-23 11:58:47 +0200

    add exercise5

--- partie2.py
+++ partie2.py
@@ -13,7 +13,6 @@ print(space + '"')
 #exercise2 : Matrix in the terminal
 suite="1000 0100 0010 0001 "
 dashes='"-------"'
-
 for i,j in enumerate(suite):
     if i% 5==4:
         print(dashes)
@@ -34,3 +33,13 @@ def factorial(n):
     else:
         return n*factorial(n-1)
 print(factorial(5))
+
+#exercise5 :the dashes count
+text=input("please enter your sentence")
+def replace():
+    text1=text.replace("-","\_")
+    print(text1)
+    if text=="":
+        print("you write nothing")
+
+replace()
