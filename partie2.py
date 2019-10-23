a0d12502d95f153f06b72a6d7b5d49840d1c1ab9
Kevin <kevin.billet.pro@gmail.com>
2019-10-23 10:49:48 +0200

    add exercise2

--- partie2.py
+++ partie2.py
@@ -1,10 +1,36 @@
+import math
 #exercise1 : a chessboard
 white=" # # # # # # # #"
 black="# # # # # # # #"
 space="   "
-
+print(space + '"')
 for i in range(1,9):
     if i % 2==0:
-        print(space+black)
+        print(space + black)
     else :
-        print(space+white)
+        print(space + white)
+print(space + '"')
+#exercise2 : Matrix in the terminal
+suite="1000 0100 0010 0001 "
+dashes='"-------"'
+
+for i,j in enumerate(suite):
+    if i% 5==4:
+        print(dashes)
+    else:
+        print(j)
+
+#exercise3 : even number ?
+def even(number):
+   if number % 2==0:
+       print("true")
+   else:
+       print("false")
+even(138)
+#exercise4 : you say factorial ?
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(5))
